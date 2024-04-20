27b00f05061764320b06e6b3363aa5ebb8722358
Achraf-Aouam <a.aouam@aui.ma>
2024-04-20 21:44:39 +0000

    extended with UID and role

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { getUserById } from "./data/user";
 import { db } from "@/lib/db";
+import { UserRole } from "@prisma/client";
 
 export const {
   handlers: { GET, POST },
@@ -14,9 +16,19 @@ export const {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) {
+        return token;
+      }
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
       return token;
     },
   },
